63db7a39ad3cf76f492d299ca700ba53b614e07c
ljn <467337035@qq.com>
2018-09-21 15:59:51 +0800

    完善个人页面信息展示

--- src/component/my/My.js
+++ src/component/my/My.js
@@ -1,11 +1,69 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import {
+    Result,
+    List,
+    WhiteSpace,
+    Modal,
+} from 'antd-mobile'
+import browserCookies from 'browser-cookies';
+
+@connect(
+    state => state.user,
+
+)
 
 export default class My extends Component {
+
+    constructor(props){
+        super(props);
+        this.logOut = this.logOut.bind(this);
+    }
+
+    // 退出登录
+    logOut = () => {
+        const mAlert = Modal.alert;
+        mAlert('注销', '确认退出？', [
+            {text:'取消', onPress: () => {}},
+            {text:'确认', onPress: () => {}}
+        ])
+
+
+        // // 直接删除当前的cookie，简单粗暴
+        // browserCookies.erase('userid');
+    }
+
     render() {
-        return (
+        const props = this.props;
+        const ListItem = List.Item
+        const Brief = List.Item.Brief
+        return props.user ? (
             <div>
-                
+                <Result 
+                    img={<img style={{width:50}} src={require(`../../res/images/${this.props.avatar}.png`)} alt=""/>}
+                    title={this.props.user}
+                    message={props.type === 'boss' ? props.company : ''}
+                />
+                <List
+                    renderHeader={() => '简介'}
+                >
+                    <ListItem
+                        multipleLine
+                    >
+                        {props.title}
+                        {props.desc.split('\n').map(v => (
+                            <Brief key={v} >{v}</Brief>
+                        ))}
+                        {props.salary ? <Brief>薪资:{props.salary}</Brief> : null}
+                    </ListItem>
+                </List>
+                <WhiteSpace></WhiteSpace>
+                <List>
+                    <ListItem onClick={() => this.logOut()} >
+                        退出登录
+                    </ListItem>
+                </List>
             </div>
-        )
+        ) : null
     }
 }
