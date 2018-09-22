0d7d8af1ee47a56a586fc843a35effc9f7c4ee07
ljn <467337035@qq.com>
2018-09-22 10:51:47 +0800

    引入socket.io，并与express绑定

--- server/server.js
+++ server/server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const userRouter = require('./user');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+
 const app = express();
+// work with express
+const server = require('http').Server(app);
+const io = require('socket.io');
+
 
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use('/user', userRouter);
 
-app.listen(9093, function(){
+server.listen(9093, function(){
     console.log("Node app start at port 9093");
 })
